899b7f8bd594c6647736ee8a44a4fab6a1e5321a
Nedim Akar <nedim.akar53411@gmail.com>
2022-03-19 13:00:31 +0300

    chore: filter endpoint and combined in one endpoint

--- src/index.ts
+++ src/index.ts
@@ -20,29 +20,82 @@ app.use(async (req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.send(DB);
+    const queries = {
+        country: req.query.country as string,
+        winary: req.query.winary as string,
+        color: req.query.color as string,
+    };
+
+    switch (queries) {
+        default:
+            res.status(400).send("missing query");
+            break;
+    }
 });
 
-app.get("/search", (req, res) => {
-    const { title } = req.query;
+app.post("/wine", (req, res) => {
+    const wine = req.body as Wine;
+    DB.push(wine);
+    res.status(200).send(wine);
+});
 
-    if (!title) {
-        logger.error("no title founded");
-        res.status(400).send("missing title");
+app.get("/wines", (req, res) => {
+    const { title, country, winery, color } = req.query;
+
+    if (title) {
+        let foundWine = DB.find((wine) => title === wine.title);
+        logger.info(`wine ${title} is searched`);
+
+        if (!foundWine) {
+            logger.error(`wine ${title} is not found`);
+            res.status(404).send("not found");
+            return;
+        }
+
+        logger.info(`found ${foundWine.id}`);
+        res.send(foundWine);
         return;
     }
 
-    const foundWine = DB.find((wine) => title === wine.title);
-    logger.info(`wine ${title} is searched`);
+    if (country) {
+        let foundWine = DB.filter((wine) => country === wine.country);
+        logger.info(`wine ${country} is searched`);
 
-    if (!foundWine) {
-        logger.error(`wine ${title} is not found`);
-        res.status(404).send("not found");
+        if (!foundWine) {
+            logger.error(`wine ${country} is not found`);
+            res.status(404).send("not found");
+            return;
+        }
+
+        logger.info(`found ${foundWine.length}`);
+        res.send(foundWine);
         return;
     }
 
-    logger.info(`found ${foundWine.id}`);
-    res.send(foundWine);
+    if (color) {
+        let foundWine = DB.filter((wine) => color === wine.color);
+        logger.info(`wine ${color} is searched`);
+
+        if (!foundWine) {
+            logger.error(`wine ${color} is not found`);
+            res.status(404).send("not found");
+            return;
+        }
+
+        logger.info(`found ${foundWine.length}`);
+        res.send(foundWine);
+        return;
+    }
+
+    if (winery) {
+        logger.error("winery is not supported");
+        res.status(400).send("winery query is not supported");
+        return;
+    }
+
+    logger.error("no query founded");
+    res.status(400).send("missing query");
+    return;
 });
 
 app.listen(CONSTANTS.PORT, () => {
--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -6,6 +6,7 @@ export const logger = winston.createLogger({
     defaultMeta: { service: CONSTANTS.SERVICE_NAME },
     transports: [
         new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+        new winston.transports.File({ filename: "logs/info.log", level: "info" }),
         new winston.transports.File({ filename: "logs/combined.log" }),
     ],
 });
